26bc6aa725f545c7b402a7effe81ca909fd84c3c
Dominik G <goldberg.dominik@gmail.com>
2016-12-09 14:16:12 +0100

    first commit on master branch

---
+++ app/src/main/java/com/goldproductions/dominik/letsgohiking/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.goldproductions.dominik.letsgohiking
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.annotation.Nullable
+
+class MainActivity : Activity() {
+
+    override fun onCreate(@Nullable savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+    }
+}
