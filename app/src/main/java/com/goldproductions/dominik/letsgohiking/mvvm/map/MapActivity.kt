1d6b91d16006da078164b113ca5bbafc91beddd9
Dominik G <goldberg.dominik@gmail.com>
2017-01-11 18:10:43 +0100

    add toolbar

---
+++ app/src/main/java/com/goldproductions/dominik/letsgohiking/mvvm/map/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.goldproductions.dominik.letsgohiking.mvvm.map
+
+import android.os.Bundle
+import com.goldproductions.dominik.letsgohiking.mvvm.base.BaseActivity
+
+class MapActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
